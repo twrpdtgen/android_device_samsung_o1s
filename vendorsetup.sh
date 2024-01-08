0c4555e246c213bdf943b158c770c1d74f44052d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-08 23:11:19 +0100

    o1s: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-G991B Script version: 3.0.0

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_o1s-user
+add_lunch_combo omni_o1s-userdebug
+add_lunch_combo omni_o1s-eng
